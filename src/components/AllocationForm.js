cd3166514d37a99176234c3e51ad27bf4d86ec3b
fahrimnabhan <fm.nabhan@rocketmail.com>
2024-04-17 20:40:54 -0400

    message

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch, remaining  } = useContext(AppContext);
+    const { dispatch, remaining, Currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -11,7 +11,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  "+remaining);
+                alert("The value cannot exceed remaining funds  "+Currency+remaining);
                 setCost("");
                 return;
             }
@@ -57,15 +57,17 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                         <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-                    
+                    <div style={{ marginLeft: '2em' , size: 10}}>
+                        {Currency}
+                    </div>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
-                            
+                           
                     </input>
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,35 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, Currency } = useContext(AppContext);
+    const { dispatch, budget, Currency, remaining} = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const upperLimit = 20000;
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && newBudget <= upperLimit) {
+            dispatch({ type: "SET_BUDGET", payload: newBudget});
+        } else if (newBudget > upperLimit) {
+            alert("The budget cannot exceed "+Currency+"20.000")
+        } else if (remaining < 10) {
+            alert("You cannot reduce the budget value lower than the spending")
+        }
+    }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: {Currency}{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: {Currency}</span>
+<input 
+    className="w-50 ms-1"
+    required
+    type="number"
+    id="cost" 
+    step="10" 
+    value={newBudget} 
+    onKeyDown={handleKeyDown}
+    onChange={handleBudgetChange}
+    >
+</input>
 </div>
     );
 };
